719b85f595c85a56ad8e74d3a9c875783903f246
Waqas Naseem <waqas.naseem.dev@gmail.com>
2021-11-08 21:08:26 +0500

    Initial Commit

---
+++ LatestMovieStoreUITests/LatestMovieStoreUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LatestMovieStoreUITestsLaunchTests.swift
+//  LatestMovieStoreUITests
+//
+//  Created by Waqas Naseem on 11/8/21.
+//
+
+import XCTest
+
+class LatestMovieStoreUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
